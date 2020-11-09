cc2cc09bd0eab234af1ebfda11475ef42e15bcf6
mohammadshahed <silentshahed@gmail.com>
2020-11-09 16:06:55 +0600

    first commit

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBimieRkVwJQ7LfegqUq9zjxv_ZEsvwEr8",
+  authDomain: "doctors-portal-shahed.firebaseapp.com",
+  databaseURL: "https://doctors-portal-shahed.firebaseio.com",
+  projectId: "doctors-portal-shahed",
+  storageBucket: "doctors-portal-shahed.appspot.com",
+  messagingSenderId: "962009576238",
+  appId: "1:962009576238:web:5e1fbcfaebb8c01002c0bf"
+};
+  export default firebaseConfig;
